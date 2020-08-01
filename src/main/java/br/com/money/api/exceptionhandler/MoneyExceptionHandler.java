247b899ef0c8130c90848f1299c379dfa9592b3a
Pedro Lima <pedrofelixcfn@gmail.com>
2020-08-01 20:49:24 -0300

    handler de salvar lancamento impl

--- src/main/java/br/com/money/api/exceptionhandler/MoneyExceptionHandler.java
+++ src/main/java/br/com/money/api/exceptionhandler/MoneyExceptionHandler.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -46,7 +49,19 @@ public class MoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
 	}
 
-	// Cria a Lista de Erros 
+	@ExceptionHandler({ DataIntegrityViolationException.class })
+	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex,
+			WebRequest request) {
+
+		String mensagemUsusario = messageSource.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
+		String mensagemDev = ExceptionUtils.getRootCauseMessage(ex);
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsusario, mensagemDev));
+
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+		
+	}
+
+	// Cria a Lista de Erros
 	private List<Erro> criarListaDeErro(BindingResult bindingResult) {
 		List<Erro> erros = new ArrayList<>();
 
@@ -56,13 +71,11 @@ public class MoneyExceptionHandler extends ResponseEntityExceptionHandler {
 			String mensagemDev = fieldError.toString();
 
 			erros.add(new Erro(mensagemUsuario, mensagemDev));
-
 		}
 
 		return erros;
 	}
-	
-	
+
 	// Class estatica de erro
 	public static class Erro {
 
