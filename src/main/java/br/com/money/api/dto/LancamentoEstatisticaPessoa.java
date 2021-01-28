4b35656cb475ed3c27f668d91b99633f0aee2d28
Pedro Lima <pedrofelixcfn@gmail.com>
2021-01-27 21:33:08 -0300

    "adicionado dto para relatorio"

---
+++ src/main/java/br/com/money/api/dto/LancamentoEstatisticaPessoa.java
@@ -0,0 +1,43 @@
+package br.com.money.api.dto;
+
+import br.com.money.api.model.Pessoa;
+import br.com.money.api.model.TipoLancamento;
+
+import java.math.BigDecimal;
+
+public class LancamentoEstatisticaPessoa {
+
+    private TipoLancamento tipo;
+    private Pessoa pessoa;
+    private BigDecimal total;
+
+    public LancamentoEstatisticaPessoa(TipoLancamento tipo, Pessoa pessoa, BigDecimal total) {
+        this.tipo = tipo;
+        this.pessoa = pessoa;
+        this.total = total;
+    }
+
+    public TipoLancamento getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoLancamento tipo) {
+        this.tipo = tipo;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+}
